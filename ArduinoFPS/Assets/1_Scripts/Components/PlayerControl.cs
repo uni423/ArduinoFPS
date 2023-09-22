e22e451e4bc416e6447e1a2efbef80ffaef5fa9e
DESKTOP-7HRLJFM\uniki <uni423@naver.com>
2023-09-22 16:33:09 +0900

    Map, Enemy Character Asset 2종 추가. 프로젝트 파일 네이밍 정리.

---
+++ ArduinoFPS/Assets/1_Scripts/Components/PlayerControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float turnSpeed = 4.0f; // 마우스 회전 속도
+
+    private float xRotate = 0.0f; // 내부 사용할 X축 회전량은 별도 정의 ( 카메라 위 아래 방향 )
+
+    void Update()
+    {
+        MouseRotation();
+    }
+
+    void MouseRotation()
+    {
+        float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed;
+        float yRotate = transform.eulerAngles.y + yRotateSize;
+
+        float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed;
+        xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);
+
+        transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+    }
+}
