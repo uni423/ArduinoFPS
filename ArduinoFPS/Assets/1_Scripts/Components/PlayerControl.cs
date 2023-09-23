a5d0f7530de3472212b489ec97f4acb193e9d0a1
unis <uni423@naver.com>
2023-09-23 21:19:28 +0900

    모바일 환경 자이로 센서 통한 화면 움직임 추가.

--- ArduinoFPS/Assets/1_Scripts/Components/PlayerControl.cs
+++ ArduinoFPS/Assets/1_Scripts/Components/PlayerControl.cs
@@ -8,19 +8,38 @@ public class PlayerControl : MonoBehaviour
 
     private float xRotate = 0.0f; // 내부 사용할 X축 회전량은 별도 정의 ( 카메라 위 아래 방향 )
 
+    private Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
+    void Start()
+    {
+        StartCoroutine(InitializeGyro());
+    }
+
+    IEnumerator InitializeGyro()
+    {
+        yield return new WaitForSeconds(1f);
+        GyroManager.Instance.EnableGyro();
+        //Input.gyro.enabled = true;
+        //yield return null;
+        //Debug.Log(Input.gyro.attitude); // attitude has data now
+    }
+
     void Update()
     {
-        MouseRotation();
+        transform.localRotation = GyroManager.Instance.GetGyroRotation() * baseRotation;
+        //MouseRotation();
     }
 
     void MouseRotation()
     {
-        float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed;
-        float yRotate = transform.eulerAngles.y + yRotateSize;
+        //float yRotateSize = Input.GetAxis("Mouse X") * turnSpeed;
+        //float yRotate = transform.eulerAngles.y + yRotateSize;
 
-        float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed;
-        xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);
+        //float xRotateSize = -Input.GetAxis("Mouse Y") * turnSpeed;
+        //xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);
 
-        transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+        //transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+        //transform.Rotate(Input.gyro.rotationRateUnbiased.x, Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z);
+        //Debug.Log(Input.gyro.attitude); // attitude has data now
+
     }
 }
